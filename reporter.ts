c871f84931a23987d058e7a575e35d0093b65d9a
jpbarto <jason.p.barto@gmail.com>
2024-11-19 00:49:14 +0000

    formatted TS

--- reporter.ts
+++ reporter.ts
@@ -8,13 +8,13 @@ export class Reporter {
     intervalId: any;
     firstReport: number = 0;
 
-    constructor (server: Server, clients: Array<Client>, reportInterval: number) {
+    constructor(server: Server, clients: Array<Client>, reportInterval: number) {
         this.server = server;
         this.clients = clients;
-        this.reportInterval = reportInterval*1000;
+        this.reportInterval = reportInterval * 1000;
     }
 
-    printReport () {
+    printReport() {
         if (this.firstReport == 0) {
             this.firstReport = Date.now();
         }
@@ -23,24 +23,24 @@ export class Reporter {
         for (const client of this.clients) {
             retryCount += client.retryCount;
         }
-        console.log ('### Report', new Date().toLocaleString ());
-        console.log (this.clients.length, 'Clients');
-        console.log ("Connection attempts:", this.server.connectAttempts);
-        console.log ('Connected clients:', this.server.connectedClients);
-        console.log ("Retries:", retryCount);
+        console.log('### Report', new Date().toLocaleString());
+        console.log(this.clients.length, 'Clients');
+        console.log("Connection attempts:", this.server.connectAttempts);
+        console.log('Connected clients:', this.server.connectedClients);
+        console.log("Retries:", retryCount);
     }
 
-    printFinalReport () {
-        this.printReport ();
-        console.log ("All clients connected in ", Date.now () - this.firstReport, "ms");
+    printFinalReport() {
+        this.printReport();
+        console.log("All clients connected in ", Date.now() - this.firstReport, "ms");
     }
 
-    startReporting () {
-        this.printReport ();
+    startReporting() {
+        this.printReport();
         this.intervalId = setInterval(this.printReport.bind(this), this.reportInterval);
     }
 
-    stopReporting () {
+    stopReporting() {
         clearInterval(this.intervalId);
     }
 }
--- server.ts
+++ server.ts
@@ -6,28 +6,28 @@ export class Server {
     maxConnectTime: number;
     connLimit: number;
 
-    constructor (minConnectTime: number, maxConnectTime: number, connLimit: number) {
+    constructor(minConnectTime: number, maxConnectTime: number, connLimit: number) {
         this.minConnectTime = minConnectTime;
         this.maxConnectTime = maxConnectTime;
         this.connLimit = connLimit;
     }
 
-    connect (handler: (result: boolean) => any) {
+    connect(handler: (result: boolean) => any) {
         this.connectAttempts++;
 
         if (this.connectingClients >= this.connLimit) {
             setTimeout(() => {
                 handler(false);
             }, 0);
-        }else{
-            const connectTime = (this.minConnectTime + 
-                (this.maxConnectTime - this.minConnectTime)*Math.random())
-                *1000;
+        } else {
+            const connectTime = (this.minConnectTime +
+                (this.maxConnectTime - this.minConnectTime) * Math.random())
+                * 1000;
 
             setTimeout(() => {
                 this.connectingClients--;
                 this.connectedClients++;
-                handler (true);
+                handler(true);
             }, connectTime);
 
             this.connectingClients++;
